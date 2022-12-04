a6bf9879826ef4a207c9d782b0faa24cb08b7879
mira-pls <miko004@student.kristiania.no>
2022-12-04 15:25:36 +0100

    uncommented bucket code

--- src/main/java/no/shoppifly/NaiveCartImpl.java
+++ src/main/java/no/shoppifly/NaiveCartImpl.java
@@ -1,11 +1,20 @@
 package no.shoppifly;
 
+import io.micrometer.core.instrument.Gauge;
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.*;
 
 @Component
 class NaiveCartImpl implements CartService {
+    MeterRegistry meterRegistry;
+
+    @Autowired
+    public NaiveCartImpl(MeterRegistry meterRegistry){
+        this.meterRegistry = meterRegistry;
+    }
 
     private final Map<String, Cart> shoppingCarts = new HashMap<>();
 
@@ -20,12 +29,23 @@ class NaiveCartImpl implements CartService {
             cart.setId(UUID.randomUUID().toString());
         }
         shoppingCarts.put(cart.getId(), cart);
+        Gauge.builder("carts", shoppingCarts, b -> b.values().size()).register(meterRegistry);
+        Gauge.builder("cartsvalue", shoppingCarts, b -> b.values().stream()
+                .flatMap(c -> c.getItems().stream()
+                        .map(i -> i.getUnitPrice() * i.getQty()))
+                .reduce(0f, Float::sum)).register(meterRegistry);
         return shoppingCarts.put(cart.getId(), cart);
     }
 
     @Override
     public String checkout(Cart cart) {
+        meterRegistry.counter("checkout").increment();
         shoppingCarts.remove(cart.getId());
+        Gauge.builder("carts", shoppingCarts, b -> b.values().size()).register(meterRegistry);
+        Gauge.builder("cartsvalue", shoppingCarts, b -> b.values().stream()
+                .flatMap(c -> c.getItems().stream()
+                        .map(i -> i.getUnitPrice() * i.getQty()))
+                .reduce(0f, Float::sum)).register(meterRegistry);
         return UUID.randomUUID().toString();
     }
 
