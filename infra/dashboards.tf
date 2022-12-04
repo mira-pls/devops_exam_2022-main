57cce11a480f31dd89e38c07eb524d9dd3cbc720
mira-pls <miko004@student.kristiania.no>
2022-12-04 15:55:41 +0100

    added relevant widgets to dashboards.tf and changed dashboard_body name to METRICWIDGETS

--- infra/dashboards.tf
+++ infra/dashboards.tf
@@ -1,7 +1,7 @@
 resource "aws_cloudwatch_dashboard" "main" {
   dashboard_name = var.candidate_id
 ## Jim; seriously! we can use any word here.. How cool is that?
-  dashboard_body = <<DEATHSTAR
+  dashboard_body = <<METRICWIDGETS
 {
   "widgets": [
     {
@@ -14,16 +14,54 @@ resource "aws_cloudwatch_dashboard" "main" {
         "metrics": [
           [
             "${var.candidate_id}",
-            "account_count.value"
+            "carts.value"
           ]
         ],
         "period": 300,
         "stat": "Maximum",
         "region": "eu-west-1",
-        "title": "Total number of accounts"
+        "title": "Number of existing carts"
+      }
+    },
+{
+      "type": "metric",
+      "x": 0,
+      "y": 0,
+      "width": 12,
+      "height": 6,
+      "properties": {
+        "metrics": [
+          [
+            "${var.candidate_id}",
+            "cartsvalue.value"
+          ]
+        ],
+        "period": 300,
+        "stat": "Maximum",
+        "region": "eu-west-1",
+        "title": "Value of all existing carts"
+      }
+    },
+{
+      "type": "metric",
+      "x": 0,
+      "y": 0,
+      "width": 12,
+      "height": 6,
+      "properties": {
+        "metrics": [
+          [
+            "${var.candidate_id}",
+            "checkout.count"
+          ]
+        ],
+        "period": 300,
+        "stat": "Maximum",
+        "region": "eu-west-1",
+        "title": "Number of carts checked out"
       }
     }
   ]
 }
-DEATHSTAR
+METRICWIDGETS
 }
